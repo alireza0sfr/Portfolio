6987009febe19826c4a7b400bf824f65ad85029f
Alireza Safari <alireza.safaree@gmail.com>
2021-08-09 22:41:55 +0430

    add breakpoint md on hamburger instead of lg

--- portfolio-frontend/src/App.vue
+++ portfolio-frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
 	<div id="app">
+		<div class="hamburger-overlay" data-bs-toggle="collapse" data-bs-target="#navmenu" @click="overlayToggler()"></div>
 		<div id="nav">
 			<!-- Navbar -->
-			<div class="hamburger-overlay" data-bs-toggle="collapse" data-bs-target="#navmenu" @click="overlayToggler()"></div>
-			<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top p-1">
+			<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top p-1">
 				<div class="container">
 					<router-link to="/" class="navbar-brand">Alireza Safari</router-link>
 					<button @click="overlayToggler()" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
@@ -13,14 +13,14 @@
 					<div class="collapse navbar-collapse" id="navmenu">
 						<ul class="navbar-nav ms-auto">
 							<li class="nav-item ms-2 pb-1 pt-1">
-								<router-link to="/" class="navbar-brand">Projects</router-link>
+								<router-link to="/" class="navbar-brand nav-link">Projects</router-link>
 							</li>
 							<li class="nav-item pb-1 pt-1">
-								<a @click="showModal()" style="cursor: pointer" class="navbar-brand d-none d-sm-inline">Hire Me</a> <!-- mobile view has its own button -->
-								<a data-bs-toggle="collapse" data-bs-target="#navmenu" @click="showModal()" style="cursor: pointer" class="navbar-brand d-sm-none">Hire Me</a>
+								<a @click="showModal()" style="cursor: pointer" class="navbar-brand nav-link d-none d-sm-inline">Hire Me</a> <!-- mobile view has its own button -->
+								<a data-bs-toggle="collapse" data-bs-target="#navmenu" @click="showModal()" style="cursor: pointer" class="navbar-brand nav-link d-sm-none">Hire Me</a>
 							</li>
 							<li class="nav-item pb-1 pt-1">
-								<a href="../assets/alireza-safari-resume.pdf" target="_blank" class="navbar-brand">Download CV</a>
+								<a href="../assets/alireza-safari-resume.pdf" target="_blank" class="navbar-brand nav-link">Download CV</a>
 							</li>
 						</ul>
 					</div>
